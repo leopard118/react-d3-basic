2ded5ada3896499c1089af223377f7dea3e55855
leopard118 <superstar95628@gmail.com>
2024-09-21 19:43:17 +1000

    fix: update cannalField

--- src/data/blocksData.js
+++ src/data/blocksData.js
@@ -3,8 +3,8 @@ const blocksData = [];
 for (let i = 0; i < 9000; i++) {
   blocksData.push({
     id: i,
-    x: (i % 100) * 30, // Adjust spacing between blocks
-    y: Math.floor(i / 100) * 30, // Adjust spacing between blocks
+    x: (i % 100) * 10, // Adjust spacing between blocks
+    y: Math.floor(i / 100) * 10, // Adjust spacing between blocks
     status: Math.random() > 0.8 ? "solid" : "available", // Rnadomly mark some blocks as solid
     ownerInfo: {
       nickname: "Owner " + i,
